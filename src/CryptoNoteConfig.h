06f653ed313f5e2142ba728f9c14b6d07009d95a
ThirdBrain SA <pierre@duvivier.xyz>
2018-01-22 15:24:47 +0100

    Delete the genesis block after changing name
    The GENESIS_COINASE_TX_HEX was reset to 0 after name change

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "ThirdBrainCoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffff7f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101c773b2b93a48d4c6b6658737ea16fdca5f8d2c16267438b439e81b9370649c15";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
